f7dfc12f4d314039deb60e7dc50c50f99eec2d56
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2023-03-02 10:53:10 +0000

    done with 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,9 +20,18 @@ babel = Babel(app)
 #@babel.localeselector
 def get_locale() -> str:
     """Retrieves the locale for a web page"""
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
+    if 'locale' in query_table:
+        if query_table['locale'] in app.config['LANGUAGES']:
+            return query_table['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route('/')
 def get_index() -> str:
